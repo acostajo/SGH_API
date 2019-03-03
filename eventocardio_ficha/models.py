3fa67f86780b217829ee472ddfa968b70239044e
jazpanam <jazinsfranpaiva@gmail.com>
2019-03-03 12:17:09 -0300

    cambios

--- eventocardio_ficha/models.py
+++ eventocardio_ficha/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-
+from eventocardiovascular.models import EventoCardiovascular
+from ficha.models import Ficha
 
 #models.IntegerField(), models.CharField(max_length=50)
 
 class EventoCardiovascularFicha(models.Model):
     codevencardofic = models.AutoField(primary_key=True) #código interno único para el evento cardiovascular asociado a la ficha
-    codevencardio= models.IntegerField() # código interno único para el evento cardiovascular
-    codficha	= models.IntegerField() # código interno único para la ficha HA, para saber a qué ficha HA está asociada el evento cardiovascular
+    codevencardio= models.ForeignKey(EventoCardiovascular,on_delete=models.DO_NOTHING, null=True) # código interno único para el evento cardiovascular
+    #codficha	= models.IntegerField() # código interno único para la ficha HA, para saber a qué ficha HA está asociada el evento cardiovascular
+    codficha	= models.ForeignKey(Ficha,  on_delete=models.DO_NOTHING, null=True ) # código interno único para la ficha HA, para saber a qué ficha HA está asociada el evento cardiovascular 
 
     class Meta:
         ordering = ('codevencardofic',)
+
+        
--- famesficha/models.py
+++ famesficha/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
-
+from fames.models import Fames
+from ficha.models import Ficha
 #models.IntegerField(), models.CharField(max_length=50)
 
 class FamesFicha(models.Model):
 
     codfameficha = models.AutoField(primary_key=True) #	código interno único para fame asociada a la ficha HA
-    codfame = models.IntegerField() #código interno único para el fame
-    codficha = models.IntegerField() #código interno único para la ficha HA, para saber a qué ficha está asociada el fame
+    codfame = models.ForeignKey(Fames,  on_delete=models.DO_NOTHING, null=True )#código interno único para el fame
+    codficha = models.ForeignKey(Ficha,  on_delete=models.DO_NOTHING, null=True ) #código interno único para la ficha HA, para saber a qué ficha está asociada el fame
     fechadesde = models.DateField() #	fecha desde de consumo del fame
     fechahasta = models.DateField() # fecha hasta de consumo del fame
 
