84cd6ffda224dcc10da9f397216e9f9e143fcac2
Joel Acosta <joacosta94@gmail.com>
2019-03-10 15:09:17 -0300

    cambios para valiodaciones de campos

--- consulta/models.py
+++ consulta/models.py
@@ -1,36 +1,48 @@
 from django.db import models
 from ficha.models import Ficha
 
-#models.IntegerField(), models.CharField(max_length=50)
+# models.IntegerField(), models.CharField(max_length=50)
+
 
 class Consulta(models.Model):
-    codconsulta	= models.AutoField(primary_key=True) #código interno único para la ficha consulta
-    codficha = models.ManyToManyField(Ficha, blank=False, null=True)
-    #codficha = models.ForeignKey(Ficha, on_delete=models.DO_NOTHING, null=True)
-    codusuario = models.IntegerField() #código interno de usuario, para saber quién agrego la ficha de consulta
-    edad = models.IntegerField(default=0) #edad del paciente
-    fechaconsulta = models.DateField(auto_now=True) #fecha en la cual se emitió la consulta
-    diagnostico = models.CharField(max_length=80) #descripción diagnostico
-    evolucion= models.CharField(max_length=80) #descripción de la evolución
-    limitacion = models.CharField(max_length=80) #descripción de las limitaciones que posee el Paciente
-    presionarte = models.IntegerField() #medida de la presión arterial
-    frescresp = models.IntegerField() #	medida de la frecuencia cardíaca
-    freccardia = models.IntegerField() #medida de la frecuencia respiratoria
-    peso = models.IntegerField() #peso
-    talla = models.IntegerField() #talla
-    nad = models.IntegerField() #número de articulaciones dolorosas
-    nat = models.IntegerField() #número de articulaciones tumefactas
-    eva	= models.IntegerField() #escala visual analógica
-    vgp	= models.IntegerField() #valoración global del Paciente
-    vgm	= models.IntegerField() #valoración global del médico
-    cdai = models.IntegerField() #clinical disease activity index
-    sdai = models.IntegerField() #simple disease activity index
-    haq = models.IntegerField() #health assessment questionnaire
-    das28pcr = models.IntegerField() #disease activity score 28 - proteína c reactiva
-    das28vsg = models.IntegerField() #disease activity score 28 - velocidad de sedimentación globular
-    sientepaci = models.IntegerField() #escala del 0 (sin dolor) al 10 (máximo dolor)
-    plan = models.CharField(max_length=80) #descripción del plan para el paciente
-    fechacreada = models.DateField(auto_now=True) #fecha de creación de la consulta
+    codconsulta = models.AutoField(
+        primary_key=True
+    )  # código interno único para la ficha consulta
+    codficha = models.ManyToManyField(Ficha, blank=False)
+    # codficha = models.ForeignKey(Ficha, on_delete=models.DO_NOTHING, null=True)
+    codusuario = (
+        models.IntegerField()
+    )  # código interno de usuario, para saber quién agrego la ficha de consulta
+    edad = models.IntegerField(default=0)  # edad del paciente
+    fechaconsulta = models.DateField(
+        auto_now=True
+    )  # fecha en la cual se emitió la consulta
+    diagnostico = models.CharField(max_length=80)  # descripción diagnostico
+    evolucion = models.CharField(max_length=80)  # descripción de la evolución
+    limitacion = models.CharField(
+        max_length=80
+    )  # descripción de las limitaciones que posee el Paciente
+    presionarte = models.IntegerField()  # medida de la presión arterial
+    frescresp = models.IntegerField()  # 	medida de la frecuencia cardíaca
+    freccardia = models.IntegerField()  # medida de la frecuencia respiratoria
+    peso = models.IntegerField()  # peso
+    talla = models.IntegerField()  # talla
+    nad = models.IntegerField()  # número de articulaciones dolorosas
+    nat = models.IntegerField()  # número de articulaciones tumefactas
+    eva = models.IntegerField()  # escala visual analógica
+    vgp = models.IntegerField()  # valoración global del Paciente
+    vgm = models.IntegerField()  # valoración global del médico
+    cdai = models.IntegerField()  # clinical disease activity index
+    sdai = models.IntegerField()  # simple disease activity index
+    haq = models.IntegerField()  # health assessment questionnaire
+    das28pcr = models.IntegerField()  # disease activity score 28 - proteína c reactiva
+    das28vsg = (
+        models.IntegerField()
+    )  # disease activity score 28 - velocidad de sedimentación globular
+    sientepaci = models.IntegerField()  # escala del 0 (sin dolor) al 10 (máximo dolor)
+    plan = models.CharField(max_length=80)  # descripción del plan para el paciente
+    fechacreada = models.DateField(auto_now=True)  # fecha de creación de la consulta
 
     class Meta:
-        ordering = ('codconsulta',)
+        ordering = ("codconsulta",)
+
--- ficha/models.py
+++ ficha/models.py
@@ -4,23 +4,31 @@ from django.db import models
 
 
 class Paciente(models.Model):
-    codpaciente = models.AutoField(primary_key=True)  # código interno único para el paciente
-    codusuario = (models.IntegerField())  # código interno de usuario, para saber quién agrego la ficha
+    codpaciente = models.AutoField(
+        primary_key=True
+    )  # código interno único para el paciente
+    codusuario = (
+        models.IntegerField()
+    )  # código interno de usuario, para saber quién agrego la ficha
     nombres = models.CharField(max_length=80)  # nombres completos del paciente
     apellidos = models.CharField(max_length=80)  # apellidos completos del paciente
-    tipodocumento=models.CharField(max_length=30, null=False,default='') 
-    nrodocumento = models.CharField(max_length=10, null=False, unique=True,default='')  # cédula de identidad del paciente
+    tipodocumento = models.CharField(max_length=30, null=False, default="")
+    nrodocumento = models.CharField(
+        max_length=10, null=False, unique=True, default=""
+    )  # cédula de identidad del paciente
     sexo = models.CharField(max_length=1)  # sexo del paciente
     fechainclusion = models.DateField()  # fecha de inclusión del paciente
-    procedencia = models.CharField(max_length=80)  # procedencia del paciente
-    nacionalidad = models.CharField(max_length=80)  # nacionalidad del paciente
-    escolaridad = models.CharField(max_length=80)  # escolaridad del paciente
+    procedencia = models.CharField(max_length=80,blank=True,null=True)  # procedencia del paciente
+    nacionalidad = models.CharField(max_length=80,blank=True,null=True)  # nacionalidad del paciente
+    escolaridad = models.CharField(max_length=80,blank=True,null=True)  # escolaridad del paciente
     diagnostico = models.CharField(max_length=80)  # diagnóstico inicial del paciente
     fechadiagnos = models.DateField()  # fecha del diagnostico
-    fechanaci = models.DateField()  # fecha de nacimiento del paciente
-    estadocivil = models.CharField(max_length=20)  # estado civil del paciente
-    profesion = models.CharField(max_length=20)  # profesión del paciente
-    telefono = models.CharField(max_length=20)  # número de teléfono del paciente
+    fechanaci = models.DateField(blank=True,null=True)  # fecha de nacimiento del paciente
+    estadocivil = models.CharField(
+        max_length=20, blank=True,null=True
+    )  # estado civil del paciente
+    profesion = models.CharField(max_length=20,blank=True,null=True)  # profesión del paciente
+    telefono = models.CharField(max_length=20,blank=True,null=True)  # número de teléfono del paciente
     fechacreada = models.DateField(auto_now=True)  # fecha de creación del paciente
 
     class Meta:
@@ -30,65 +38,71 @@ class Paciente(models.Model):
 class Ficha(models.Model):
     codficha = models.AutoField(primary_key=True)  # codigo de la ficha
     codpaciente = models.OneToOneField(Paciente, on_delete=models.CASCADE)
-    #codpaciente = models.ForeignKey(Paciente, null=True, blank=True)
+    # codpaciente = models.ForeignKey(Paciente, null=True, blank=True)
     ##codpaciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)  # codigo del paciente
-    codpatron = (
-        models.IntegerField()
+    codpatron = models.IntegerField(
+        null=True,blank=True
     )  # código interno único para anapatron, para saber que patron tiene asociada la ficha HA
-    codusuario = (
-        models.IntegerField()
+    codusuario = models.IntegerField(
+        null=True,blank=True
     )  # código interno de usuario, para saber quién agrego la ficha
-    nhc = (
-        models.IntegerField(unique=True)
+    nhc = models.IntegerField(
+        unique=True
     )  # número de historial clínico, código externo de la ficha, por el cual se manejan los usuarios
-    iniciosint = (
-        models.DateField()
+    iniciosint = models.DateField(
+        blank=True, null=True
     )  # Fecha en el que el Paciente empezó a notar síntomas
     formainic = models.CharField(
-        max_length=50
+        max_length=50, blank=True
     )  # Descripción de los síntomas del paciente
-    apf = models.CharField(max_length=50)  # Antecedentes Patológicos Familiares
+    apf = models.CharField(
+        max_length=50, blank=True
+    )  # Antecedentes Patológicos Familiares
     apfcv = models.CharField(
-        max_length=50
+        max_length=50, blank=True
     )  # Antecedentes patológicos familiares cardiovasculares
     appfractura = models.CharField(
-        max_length=50
+        max_length=50, blank=True
     )  # Antecedentes patológicos personales de fracturas
     apffractura = models.CharField(
-        max_length=50
+        max_length=50, blank=True
     )  # Antecedentes patológicos familiares de fracturas
-    protesissitio = models.CharField(max_length=50)  # Datos de prótesis del Paciente
-    protefecha = models.DateField()  # Datos de prótesis del Paciente
+    protesissitio = models.CharField(
+        max_length=50, blank=True
+    )  # Datos de prótesis del Paciente
+    protefecha = models.DateField(
+        null=True, blank=True
+    )  # Datos de prótesis del Paciente
     apfneoplasias = models.CharField(
-        max_length=50
+        max_length=50, blank=True
     )  # Antecedentes familiares de neoplasias (tumores)
     sedentarismo = models.BooleanField()  # Si el Paciente es sedentario
     actifisica = models.BooleanField()  # Si el Paciente realiza actividad física
-    #tabaquismo = models.BooleanField(null=True)  # Si el Paciente es tabaquista
-    tabaqfecha = models.DateField()  # Fecha que comenzo a fumar
-    tabnumero = models.IntegerField()  # Número  de paquetes que fuma/fumo por dia
+    # tabaquismo = models.BooleanField(null=True)  # Si el Paciente es tabaquista
+    tabaqfecha = models.DateField(blank=True,null=True)  # Fecha que comenzo a fumar
+    tabnumero = models.IntegerField(null=True,blank=True)
     extabaq = models.BooleanField()  # Si fue fumador
     menarca = models.IntegerField()  # Edad de primera menstruación
     menopausia = models.IntegerField()  # Edad de menopausia
-    edadvidasex = models.IntegerField() # A los cuantos años comenzó a tener actividad sexual
+    edadvidasex = models.IntegerField(null=True,blank=True)
     gestas = models.IntegerField()  # Cantidad de gestas
     partos = models.IntegerField()  # Cantidad de partos
     cesareas = models.IntegerField()  # Cantidad de Cesáreas
     abortos = models.IntegerField()  # Cantidad de abortos
     hisjospost = models.BooleanField()  # sí o no, tuvo hijos
-    factorreuma_pos = models.CharField(max_length=50,default='')  # factor reumatoide
-    factorreuma_neg = models.CharField(max_length=50,default='')
-    factorreuma_nivel = models.CharField(max_length=50,default='')
-    acp_pos = models.CharField(max_length=50,default='')  # factor reumatoide
-    acp_neg = models.CharField(max_length=50,default='')
-    acp_nivel = models.CharField(max_length=50,default='')
-    ana_pos = models.CharField(max_length=50,default='')  # factor reumatoide
-    ana_neg = models.CharField(max_length=50,default='')
-    ana_patron = models.CharField(max_length=50,default='')
+    factorreuma_pos = models.CharField(max_length=50, blank=True)  # factor reumatoide
+    factorreuma_neg = models.CharField(max_length=50, blank=True)
+    factorreuma_nivel = models.CharField(max_length=50, blank=True)
+    acp_pos = models.CharField(max_length=50, blank=True)  # factor reumatoide
+    acp_neg = models.CharField(max_length=50, blank=True)
+    acp_nivel = models.CharField(max_length=50, blank=True)
+    ana_pos = models.CharField(max_length=50, blank=True)  # factor reumatoide
+    ana_neg = models.CharField(max_length=50, blank=True)
+    ana_patron = models.CharField(max_length=50, blank=True)
     rxmanos = models.BooleanField()  # erecciones sí o no
-    rxmanosfecha = (models.DateField())  # la fecha que tuvo las erecciones ----------> wtf erecciones hei
+    rxmanosfecha = models.DateField(blank=True,null=True)
     rxpies = models.BooleanField()  # erecciones sí o no
-    rxpiesfecha = models.DateField()  # la fecha que tuvo las erecciones
+    rxpiesfecha = models.DateField(blank=True,null=True)
     fechacreadaa = models.DateField(auto_now=True)  # fecha de creación de la ficha HA
 
     class Meta:
