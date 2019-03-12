4207dc29fb64a0060e8d1b149f640a83cca060a9
jazmin <jazinsfranpaiva@gmail.com>
2019-03-12 19:38:03 -0300

    api de consultas

--- consulta/models.py
+++ consulta/models.py
@@ -5,30 +5,31 @@ from ficha.models import Ficha
 
 
 class Consulta(models.Model):
-    codconsulta = models.AutoField(
-        primary_key=True
-    )  # código interno único para la ficha consulta
-    codficha = models.ManyToManyField(Ficha, blank=False)
+    # código interno único para la ficha consulta
+    codconsulta = models.AutoField(primary_key=True)
     # codficha = models.ForeignKey(Ficha, on_delete=models.DO_NOTHING, null=True)
-    codusuario = (
-        models.IntegerField()
-    )  # código interno de usuario, para saber quién agrego la ficha de consulta
+    codficha = models.ForeignKey(
+        Ficha, on_delete=models.DO_NOTHING, null=False, default=0)
+    # código interno de usuario, para saber quién agrego la ficha de consulta
+    codusuario = (models.IntegerField())
     edad = models.IntegerField(default=0)  # edad del paciente
-    fechaconsulta = models.DateField(
-        auto_now=True
-    )  # fecha en la cual se emitió la consulta
+    # fecha en la cual se emitió la consulta
+    fechaconsulta = models.DateField(auto_now=True)
     diagnostico = models.CharField(max_length=80)  # descripción diagnostico
+    tratamientoactual = models.CharField(
+        max_length=200, blank=True)  # descripción tratamiento actual
     evolucion = models.CharField(max_length=80)  # descripción de la evolución
-    limitacion = models.CharField(
-        max_length=80
-    )  # descripción de las limitaciones que posee el Paciente
+    # descripción de las limitaciones que posee el Paciente
+    limitacion = models.CharField(max_length=80)
+    limitacionmotivo = models.CharField(max_length=80)
     presionarte = models.IntegerField()  # medida de la presión arterial
-    frescresp = models.IntegerField()  # 	medida de la frecuencia cardíaca
+    frescresp = models.IntegerField()  # medida de la frecuencia cardíaca
     freccardia = models.IntegerField()  # medida de la frecuencia respiratoria
     peso = models.IntegerField()  # peso
     talla = models.IntegerField()  # talla
     nad = models.IntegerField()  # número de articulaciones dolorosas
-    nat = models.IntegerField()  # número de articulaciones tumefactas
+    # número de articulaciones tumefactas kore de quien era ese migrate q estaba abierto?, no s, de consulta o de ficha nomas va ser
+    nat = models.IntegerField()
     eva = models.IntegerField()  # escala visual analógica
     vgp = models.IntegerField()  # valoración global del Paciente
     vgm = models.IntegerField()  # valoración global del médico
@@ -36,13 +37,15 @@ class Consulta(models.Model):
     sdai = models.IntegerField()  # simple disease activity index
     haq = models.IntegerField()  # health assessment questionnaire
     das28pcr = models.IntegerField()  # disease activity score 28 - proteína c reactiva
-    das28vsg = (
-        models.IntegerField()
-    )  # disease activity score 28 - velocidad de sedimentación globular
-    sientepaci = models.IntegerField()  # escala del 0 (sin dolor) al 10 (máximo dolor)
-    plan = models.CharField(max_length=80)  # descripción del plan para el paciente
-    fechacreada = models.DateField(auto_now=True)  # fecha de creación de la consulta
+    # disease activity score 28 - velocidad de sedimentación globular
+    das28vsg = (models.IntegerField())
+    # escala del 0 (sin dolor) al 10 (máximo dolor)
+    sientepaci = models.IntegerField()
+    # descripción del plan para el paciente
+    plan = models.CharField(max_length=80)
+    # fecha de creación de la consulta
+
+    fechacreada = models.DateField(auto_now=True)
 
     class Meta:
         ordering = ("codconsulta",)
-
